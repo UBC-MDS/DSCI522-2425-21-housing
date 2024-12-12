2447039fddb5d1daf82367a1a2a270add7e07533
Thamerd <thamerd@student.ubc.ca>
2024-12-12 15:30:14 -0800

    test_load_data done

---
+++ tests/test_load_data.py
@@ -0,0 +1,22 @@
+import pytest
+import pandas as pd
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from src.load_data import load_csv
+
+# Load csv should use pandas to read a CSV from a secure url then save the raw csv to a given path
+
+def test_load_csv():
+    # Example csv url for testing purposes
+    url = "https://gist.githubusercontent.com/dsternlicht/74020ebfdd91a686d71e785a79b318d4/raw/d3104389ba98a8605f8e641871b9ce71eff73f7e/chartsninja-data-1.csv"
+    expected = pd.read_csv(url)
+    write_to = "../data/raw"
+    filename = "test_data.csv"
+
+    # Running load_csv to let it write the csv from the url
+    load_csv(url, write_to, filename)
+    actual = pd.read_csv(os.path.join(write_to, filename))
+
+    assert actual.equals(expected)
